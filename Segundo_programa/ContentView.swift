0664b516f457d427a2b1b2729e68769062fb9580
alumno <alumno@gmail.com>
2024-09-20 12:12:50 +0200

    Initial Commit

---
+++ Segundo_programa/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Segundo_programa
+//
+//  Created by alumno on 20/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Segundo_programa/Segundo_programaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Segundo_programaApp.swift
+//  Segundo_programa
+//
+//  Created by alumno on 20/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Segundo_programaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
